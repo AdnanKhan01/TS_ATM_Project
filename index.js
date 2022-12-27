ad35c5a9bf48dddc33bac1bc161aad9dfffb3339
Adnan Khan <adnankhan3241@yahoo.com>
2022-12-28 00:29:09 +0500

    Project files

---
+++ index.js
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+import users from "./users.js";
+var balance = 100000;
+async function atm() {
+    let ans1 = await inquirer.prompt([
+        {
+            name: "user_name",
+            type: "input",
+            message: "Please Enter Your Name:",
+        }
+    ]);
+    let userExist = users.findIndex((obj) => obj.name == ans1.user_name);
+    if (userExist == -1) {
+        console.log("Sorry!! User doest not exist");
+    }
+    else {
+        startAgain();
+    }
+}
+async function askQuestions() {
+    var ans = await inquirer.prompt([
+        {
+            type: "password",
+            name: "PIN",
+            message: "Please enter your PIN: "
+        },
+        {
+            type: "list",
+            name: "accountType",
+            choices: ["Current", "Savings"],
+            message: "Select your account type: ",
+        },
+        {
+            type: "list",
+            name: "operations",
+            choices: ["Fast Cash", "Enter amount", "Balance check"],
+            message: "Select operation: ",
+            when(ans) {
+                return ans.accountType;
+            }
+        },
+        {
+            type: "list",
+            name: "fast_c",
+            choices: [1000, 2000, 5000, 10000, 25000],
+            message: "Select amount: ",
+            when(ans) {
+                return ans.operations == "Fast Cash";
+            }
+        },
+        {
+            type: "number",
+            name: "amount",
+            message: "Enter the amount you want to withdraw: ",
+            when(ans) {
+                return ans.operations == "Enter amount";
+            },
+        },
+        {
+            type: "input",
+            name: "bal",
+            message: "Type yes if you want to check your balance ",
+            when(ans) {
+                return ans.operations == "Balance check";
+            }
+        }
+    ]).then((check) => {
+        if (check.amount > balance) {
+            console.log("The amount you entered is greater than your balance");
+        }
+        else if (check.fast_c) {
+            balance = balance - check.fast_c;
+            console.log("Here is your ", check.fast_c);
+            console.log("Your remaining balance is ", balance);
+        }
+        else if (check.amount) {
+            balance = balance - check.amount;
+            console.log("Here you go. Here is your ", check.amount);
+            console.log("Remaining balance is ", balance);
+        }
+        else if (check.bal) {
+            console.log("Your total balance is :", balance);
+        }
+    });
+}
+async function startAgain() {
+    do {
+        await askQuestions();
+        var again = await inquirer
+            .prompt({
+            type: "input",
+            name: "doAgain",
+            message: chalk.blueBright("Do you want to perform another transaction? Type 'y' for yes and 'n' for No ")
+        });
+    } while (again.doAgain == "y" || again.doAgain == "Y");
+}
+atm();
---
+++ users.ts
@@ -0,0 +1,21 @@
+
+let users =[{
+    name: "Adnan",
+    accountNo: 1234,
+    pin:1234,
+    balance: 100000
+},
+{
+        name: "Ali",
+        accountNo: 2345,
+        pin:2345,
+        balance: 50000
+},
+{
+    
+        name: "Ahmed",
+        accountNo: 5678,
+        pin:5678,
+        balance: 40000
+}]
+export default users;
